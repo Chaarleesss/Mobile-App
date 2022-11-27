3c00a0f05eb38ebc46982544bb7f95af7b2be870
Charles Andriansyah <charlesandriansyah104@gmail.com>
2022-11-27 13:48:43 +0700

    Add  profile

--- App.js
+++ App.js
@@ -4,22 +4,14 @@ import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import MainPages from "./Pages/MainPages";
 import SignUp from "./Pages/SignUp";
+import Profile from "./Pages/Profile";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App({ navigation }) {
   const Stack = createNativeStackNavigator();
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
-        <Stack.Screen name="MainPages" component={MainPages} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Profile />;
 }
 
 const styles = StyleSheet.create({
